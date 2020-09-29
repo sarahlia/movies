9ed2de743d3faf0e2c3330f1879bd8234495c1f1
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-09-29 14:27:41 -0500

    setting up repo for cloned project.

--- src/main/sql/setup.sql
+++ src/main/sql/setup.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS movie_db;
+CREATE DATABASE IF NOT EXISTS mov_db;
 
-CREATE USER movie_user@localhost IDENTIFIED BY '******';
-GRANT ALL ON movie_db.* TO movie_user@localhost;
+CREATE USER mov_user@localhost IDENTIFIED BY '$ometh1ng';
+GRANT ALL ON mov_db.* TO mov_user@localhost;
