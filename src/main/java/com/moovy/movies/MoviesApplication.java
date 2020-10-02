cb7d56ecf4051c7fa91843640ae39c944f6642e6
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-10-02 15:29:27 -0500

    check to see if this will merge to branch after demo.

--- src/main/java/com/moovy/movies/MoviesApplication.java
+++ src/main/java/com/moovy/movies/MoviesApplication.java
@@ -1,5 +1,6 @@
 package com.moovy.movies;
 
+import com.moovy.movies.models.Bookmark;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class MoviesApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoviesApplication.class, args);
     }
-
 }
--- src/main/sql/setup.sql
+++ src/main/sql/setup.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS movie_db;
 
-CREATE USER movie_user@localhost IDENTIFIED BY '******';
+CREATE USER movie_user@localhost IDENTIFIED BY '$ometh1ng';
 GRANT ALL ON movie_db.* TO movie_user@localhost;
